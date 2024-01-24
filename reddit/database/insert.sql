4f67fc2c7d23b2069b957fb36eb4e43e9339092b
AleksandarBudimirovic <aleksandar.budimirovic97@gmail.com>
2024-01-24 16:27:30 +0100

    Worked on html and sql

--- reddit/database/insert.sql
+++ reddit/database/insert.sql
@@ -1,56 +1,48 @@
+USE redditbase;
 
-insert into korisnik (id, korisnicko_ime, lozinka,uloga) values (1,'admin', '123', 'ADMIN' );
+insert into ban(id, timestamp, community_id, user_id) values(1, '2022-01-20', 2, 3);
+insert into ban(id, timestamp, community_id, user_id) values(2, '2022-01-20', 2, 4);
 
-use redditbase;
+insert into comment(id, is_deleted, text, timestamp, post_id, user_id, main_comment_id) values (1, 0, 'some text', '2012-02-10', 1, 1, 1);
+insert into comment(id, is_deleted, text, timestamp, post_id, user_id, main_comment_id) values (2, 0, 'some other text', '2012-02-10', 1, 1, 2);
+insert into comment(id, is_deleted, text, timestamp, post_id, user_id, main_comment_id) values (3, 0, 'another text', '2012-02-10', 1, 1, 1);
+insert into comment(id, is_deleted, text, timestamp, post_id, user_id, main_comment_id) values (4, 0, 'more text', '2012-02-10', 1, 1, 2);
+insert into comment(id, is_deleted, text, timestamp, post_id, user_id, main_comment_id) values (5, 0, 'additional text', '2012-02-10', 1, 1, 1);
+insert into comment(id, is_deleted, text, timestamp, post_id, user_id, main_comment_id) values (6, 0, 'extra text', '2012-02-10', 1, 1, 2);
 
-insert into ban (id, timestamp, community, user) values(1, '2022-01-20', 2, 3);
-insert into ban (id, timestamp, community, user) values(2, '2022-01-20', 2, 4);
+insert into community(id, creationDate, description, isSuspended, suspendedReason, user_id) values (1, '2008-02-10', 'Garden of Eve', 0, NULL, 1);
+insert into community(id, creationDate, description, isSuspended, suspendedReason, user_id) values (2, '2008-02-10', 'Cat memes', 0, NULL, 1);
+insert into community(id, creationDate, description, isSuspended, suspendedReason, user_id) values (3, '2008-02-10', 'More cat memes', 0, NULL, 2);
+insert into community(id, creationDate, description, isSuspended, suspendedReason, user_id) values (4, '2008-02-10', 'Cat memes but fast', 0, NULL, 2);
+insert into community(id, creationDate, description, isSuspended, suspendedReason, user_id) values (5, '2008-02-10', 'Programming Wizards', 0, NULL, 1);
+insert into community(id, creationDate, description, isSuspended, suspendedReason, user_id) values (6, '2008-02-10', 'Tech Enthusiasts', 0, NULL, 1);
 
-use redditbase;
-
-insert into comment(id, is_deleted, text, timestamp, post, user, main_comment) values (1, false, 'some text', '2012-02-10', 1, 1, 'This is a main comment');
-insert into comment(id, is_deleted, text, timestamp, post, user, main_comment) values (2, false, 'some other text', '2012-02-10', 1, 1, 'This is also a main comment');
-
-use redditbase;
-
-insert into community(id, creation_date, description, is_suspended, suspended_reason, user) values (1, '2008-02-10', 'Garden of Eve', false, NULL, 1);
-insert into community(id, creation_date, description, is_suspended, suspended_reason, user) values (1, '2008-02-10', 'Cat memes', false, NULL, 1);
-insert into community(id, creation_date, description, is_suspended, suspended_reason, user) values (1, '2008-02-10', 'More cat memes', false, NULL, 2);
-insert into community(id, creation_date, description, is_suspended, suspended_reason, user) values (1, '2008-02-10', 'Cat memes but fast', false, NULL, 2);
-
-user redditbase;
-
-insert into flair(id, name, community, post) values (1, 'like', 1, 1);
-insert into flair(id, name, community, post) values (2, 'like', 2, 1);
-insert into flair(id, name, community, post) values (3, 'like', 3, 1);
-insert into flair(id, name, community, post) values (4, 'like', 4, 1);
-
-user redditbase;
-
-insert into post(id, creation_date, image_path, title, text, community, user) values (1, '2022-01-20', NULL, 'Pretty_nature', 'An image of pretty nature hopefully', 1, 1);
-insert into post(id, creation_date, image_path, title, text, community, user) values (2, '2022-01-20', NULL, 'Cat video number 9487', 'New phone who dis', 2, 1);
-insert into post(id, creation_date, image_path, title, text, community, user) values (3, '2022-01-20', NULL, 'Pawsies', 'I like trains', 2, 2);
-insert into post(id, creation_date, image_path, title, text, community, user) values (4, '2022-01-20', NULL, 'Cat video number 9488', 'An image of a cat hopefully', 3, 2);
-
-user redditbase;
-
-insert into reaction(id, timestamp, type, comment, post, user) values (1, '2022-01-20', 'type1', 1, 1, 1);
-insert into reaction(id, timestamp, type, comment, post, user) values (1, '2022-01-20', 'type2', 2, 2, 2);
-
-user redditbase;
-
-insert into report(id, accepted, reason, timestamp, comment, post) values(1, false, 'False accusation', '2022-01-20', 1, 1);
-insert into report(id, accepted, reason, timestamp, comment, post) values(2, false, 'False accusation', '2022-01-20', 2, 2);
-
-user redditbase;
-
-insert into user(id, avatar, description, display_name, password, registration_date, role, username) values(1, NULL, 'Number 1 admin around', 'Admin007', 'admin', '2008-02-9', 'ADMIN', 'Admin007');
-insert into user(id, avatar, description, display_name, password, registration_date, role, username) values(2, NULL, 'Something definitely not weird', 'Jared', 'jerry', '2008-02-9', 'USER', 'jerry');
-insert into user(id, avatar, description, display_name, password, registration_date, role, username) values(3, NULL, 'The Jorris experience', 'Jorris', 'bonson', '2008-02-9', 'USER', 'Jorris');
+insert into flair(id, name, community_id, post_id) values (1, 'like', 1, 1);
+insert into flair(id, name, community_id, post_id) values (2, 'like', 2, 1);
+insert into flair(id, name, community_id, post_id) values (3, 'like', 3, 1);
+insert into flair(id, name, community_id, post_id) values (4, 'like', 4, 1);
+insert into flair(id, name, community_id, post_id) values (5, 'dislike', 1, 1);
+insert into flair(id, name, community_id, post_id) values (6, 'love', 2, 1);
 
 
+insert into post(id, creationDate, imagePath, title, text, community_id, user_id) values (1, '2022-01-20', NULL, 'Pretty_nature', 'An image of pretty nature hopefully', 1, 1);
+insert into post(id, creationDate, imagePath, title, text, community_id, user_id) values (2, '2022-01-20', NULL, 'Cat video number 9487', 'New phone who dis', 2, 1);
+insert into post(id, creationDate, imagePath, title, text, community_id, user_id) values (3, '2022-01-20', NULL, 'Pawsies', 'I like trains', 2, 2);
+insert into post(id, creationDate, imagePath, title, text, community_id, user_id) values (4, '2022-01-20', NULL, 'Cat video number 9488', 'An image of a cat hopefully', 3, 2);
+insert into post(id, creationDate, imagePath, title, text, community_id, user_id) values (5, '2022-01-21', NULL, 'Sunset Views', 'Beautiful sunset views from my balcony', 1, 1);
+insert into post(id, creationDate, imagePath, title, text, community_id, user_id) values (6, '2022-01-21', NULL, 'Coding Life', 'Late-night coding session. Anyone else awake?', 2, 2);
 
 
+insert into reaction(id, timestamp, type, comment_id, post_id, user_id) values (1, '2022-01-20', 'type1', 1, NULL, 1);
+insert into reaction(id, timestamp, type, comment_id, post_id, user_id) values (2, '2022-01-20', 'type2', 2, NULL, 2);
+insert into reaction(id, timestamp, type, comment_id, post_id, user_id) values (3, '2022-01-21', 'type1', 1, NULL, 1);
+insert into reaction(id, timestamp, type, comment_id, post_id, user_id) values (4, '2022-01-21', 'type2', NULL, 2, 2);
+insert into reaction(id, timestamp, type, comment_id, post_id, user_id) values (5, '2022-01-22', 'type1', 1, NULL, 2);
+insert into reaction(id, timestamp, type, comment_id, post_id, user_id) values (6, '2022-01-22', 'type2', 2, NULL, 1);
 
-
-
+insert into report(id, accepted, reason, timestamp, comment_id, post_id) values(1, false, 'False accusation', '2022-01-20', 1, 1);
+insert into report(id, accepted, reason, timestamp, comment_id, post_id) values(2, false, 'False accusation', '2022-01-20', 2, 2);
+insert into report(id, accepted, reason, timestamp, comment_id, post_id) values(3, true, 'Spam', '2022-01-21', 3, NULL);
+insert into report(id, accepted, reason, timestamp, comment_id, post_id) values(4, true, 'Inappropriate content', '2022-01-21', NULL, 1);
+insert into report(id, accepted, reason, timestamp, comment_id, post_id) values(5, false, 'False accusation', '2022-01-22', 1, NULL);
+insert into report(id, accepted, reason, timestamp, comment_id, post_id) values(6, false, 'False accusation', '2022-01-22', 2, 2);
